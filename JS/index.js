86be7b0b8157302d771545c962352fe6b0fe9a3c
TakeshiKei <kevinx.tanx@gmail.com>
2025-05-08 21:28:32 +0700

    fix

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,40 @@
-let desc = document.querySelectorAll('.desc')
+document.addEventListener("DOMContentLoaded", () => {
+    const audio = document.getElementById("background-audio");
+    const musicToggle = document.getElementById("music-toggle");
+
+    // Event listener untuk toggle musik
+    musicToggle.addEventListener("click", () => {
+        if (audio.paused) {
+            audio.play().catch(() => {
+                alert("Can't play audio");
+            });
+            musicToggle.src = "./Images/music-on.png"; // Ubah ikon ke "musik hidup"
+        } else {
+            audio.pause();
+            musicToggle.src = "./Images/music-off.png"; // Ubah ikon ke "musik mati"
+        }
+    });
+});
+
+document.getElementsByClassName("toggler-navbar")[0].addEventListener("click", toggleClass);
+
+function toggleClass() {
+    document.getElementsByClassName("hamburger-menu")[0].classList.toggle('open');
+    document.getElementsByClassName("sidebar")[0].classList.toggle('open');
+}
+
+// keluar dari sidebar saat menekan area luar
+document.addEventListener("click", function (event) {
+    const sidebar = document.querySelector(".sidebar");
+    const toggler = document.querySelector(".toggler-navbar");
+
+    if (!sidebar.contains(event.target) && !toggler.contains(event.target)) {
+        sidebar.classList.remove("open");
+        document.querySelector(".hamburger-menu").classList.remove("open");
+    }
+});
+
+let desc = document.querySelectorAll('.desc');
 desc.forEach(popup => popup.addEventListener('click', () => {
     popup.classList.toggle('active')
 }))
